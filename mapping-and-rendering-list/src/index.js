91a1841c9257c11c95b7704b0a87123319494363
Alex14683 <110792798+alex14683@users.noreply.github.com>
2022-10-12 23:27:33 -0500

    Add Mapping And Rendering List Project

---
+++ mapping-and-rendering-list/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function AnimalList(props) {
+  const animals = props.animals
+  const listItems = animals.map((animal) =>
+      <li key={animal.toString()}>
+        {animal}
+      </li>
+  )
+  return (
+      <ul>{listItems}</ul>
+  )
+}
+
+const animalArray = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AnimalList animals={animalArray} />)

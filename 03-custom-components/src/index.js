f6e80753adf01d8876ec1757783cefb85bd910fc
Mr Alebrije <alebrije.developer@gmail.com>
2022-10-24 11:49:45 -0500

    03 fixes - pair programming

---
+++ 03-custom-components/src/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function Divider(){
+  return <div style={{marginTop: 10, marginBottom:10}}></div>
+}
+
+function CustomButton({id, color}){
+  return (
+      <button style={{
+        backgroundColor: color,
+        width: 150,
+        height: 60,
+        cursor: "pointer"
+      }}  onClick={showAlert}>
+        Button {id}
+      </button>
+  )
+
+  function showAlert () {
+    alert(`You clicked on Button ${id}`)
+  }
+}
+/*
+const buttonGroup = (
+    <div className="container">
+      <CustomButton id={"1"}></CustomButton>
+      <CustomButton id="2"/>
+      <CustomButton id="3"/>
+    </div>
+)
+*/
+
+const buttonConfig = [
+  {
+    id: "1",
+    key: 1,
+    color: 'green'
+  },
+  {
+    id: "2",
+    key: 2,
+    color: 'white'
+  }, {
+    id: "3",
+    key: 3,
+    color: 'red'
+  }
+]
+
+const buttonGroup = (
+    <>
+      {/* with individual props */}
+      <div className="container">
+        {
+          buttonConfig.map(({id, key, color}) => {
+            return <CustomButton key={key} id={id} color={color}/>
+          })
+        }
+      </div>
+
+      <Divider/>
+
+      {/* with spread props */}
+      <div className="container">
+        {
+          buttonConfig.map(props => {
+            return <CustomButton {...props}/>
+          })
+        }
+      </div>
+    </>
+)
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(buttonGroup)

1211ab6d2641e2993c2993cf891055cdbffc14f9
Mr Alebrije <alebrije.developer@gmail.com>
2022-10-26 11:44:27 -0500

    05 fixes - pair programming

---
+++ 05-mapping-and-rendering-list/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const animals = [
+  {name: 'dog', translation: 'perro', isQuadruped: true},
+  {name: 'cat', translation: 'gato', isQuadruped: true},
+  {name: 'chicken', translation: 'pollo', isQuadruped: false},
+  {name: 'duck', translation: 'pato', isQuadruped: false},
+  {name: 'cow', translation: 'vaca', isQuadruped: true},
+  {name: 'sheep', translation: 'oveja', isQuadruped: true},
+  {name: 'horse', translation: 'caballo', isQuadruped: true}
+]
+
+function AnimalList() {
+  return (
+      <ul>
+        {
+          animals
+          .filter(animal => !animal.isQuadruped)
+          .map((animal) => {
+            return <li key={animal.name}>
+                  {animal.name} = {animal.translation}
+                </li>
+              })
+        }
+      </ul>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AnimalList />)

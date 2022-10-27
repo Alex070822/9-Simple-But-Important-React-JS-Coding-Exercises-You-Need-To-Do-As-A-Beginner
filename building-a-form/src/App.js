8080fe545c030e0abaca73bde86112979d63af64
Mr Alebrije <alebrije.developer@gmail.com>
2022-10-27 11:31:52 -0500

    07 fixes - pair programming

--- building-a-form/src/App.js
+++ building-a-form/src/App.js
@@ -1,29 +1,40 @@
 import './App.css';
 
-function UserNameForm({onSubmitName}) {
-  function handleSubmit(event){
-    event.preventDefault()
-    console.dir(event.target.elements.firstNameInput.value)
-    const firstName = event.target.elements.firstNameInput.value
-    const lastName = event.target.elements.lastNameInput.value
-    onSubmitName ([firstName, lastName])
-  }
+function CustomForm() {
   return (
       <form onSubmit={handleSubmit}>
         <div>
-          <input id="firstNameInput" type="text" placeholder="First name"/>
+          <input id="firstNameInput"
+                 type="text"
+                 placeholder="First name"/>
         </div>
         <div>
-          <input id="lastNameInput" type="text" placeholder="Last name"/>
+          <input id="lastNameInput"
+                 type="text"
+                 placeholder="Last name"/>
         </div>
-        <button type="submit">GREET ME</button>
+        <button type="submit">Greet me</button>
       </form>
   )
 }
 
+function handleSubmit(event){
+  event.preventDefault()
+
+  const firstName = event.target.elements.firstNameInput.value
+  const lastName = event.target.elements.lastNameInput.value
+
+  const msg = getGreetingMsg(firstName, lastName)
+
+  alert(msg)
+}
+
+function getGreetingMsg(firstName, lastName){
+  return `Hello ${firstName} ${lastName}!`
+}
+
 function App() {
-  const onSubmitName = username => alert(`Hello ${username[0]} ${username[1]}!`)
-  return <UserNameForm onSubmitName={onSubmitName}/>
+  return <CustomForm/>
 }
 
 export default App;

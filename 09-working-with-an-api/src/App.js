96c4b0726b3d4fceb9e3e8975854200dc7fad61a
Mr Alebrije <alebrije.developer@gmail.com>
2022-10-31 11:49:21 -0600

    09 fixes - pair programming

---
+++ 09-working-with-an-api/src/App.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react"
+import CardInfo from "./cardInfo"
+import "./App.css"
+
+const endpointUrl = "https://random-data-api.com/api/users/random_user?size=10"
+
+function App() {
+  console.log("# Render")
+
+  const [model, setModel] = useState({
+    items: [],
+    shouldLoadItems: true,
+  })
+
+  useEffect(() => {
+    console.log("# useEffect")
+    const fetchData = async () => {
+      const response = await fetch(endpointUrl)
+      const data = await response.json()
+
+      setModel({
+        items: data,
+        shouldLoadItems: false,
+      })
+    }
+
+    if(model.shouldLoadItems){
+      fetchData()
+    }
+
+  }, [model.shouldLoadItems])
+
+  function fetchRandomData() {
+    setModel((prevState) => {
+      return {
+        ...prevState,
+        shouldLoadItems: true,
+      }
+    })
+  }
+
+  return (
+    <div className="container">
+      <button onClick={fetchRandomData}>Fetch Random</button>
+      <div className="background">
+        {
+          model.items.map((item) => (
+              <CardInfo
+                  key={item.id}
+                  avatar={item.avatar}
+                  name={`${item.first_name} ${item.last_name}`}
+                  title={item.employment.title}
+                  email={item.email}
+                  phoneNumber={item.phone_number}
+                  address={`${item.address.city}, ${item.address.state}`}
+              />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
